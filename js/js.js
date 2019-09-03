5d411918b241123c673a25c5b4ff3915fb41827d
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-09-03 09:26:00 -0500

    setting a button to get the next month

--- js/js.js
+++ js/js.js
@@ -1,2 +1,10 @@
 console.log("JavaScript is running");
 
+var nextMonthButton = document.getElementById("next-month");
+nextMonthButton.addEventListener("click", getNextMonth);
+
+function getNextMonth()
+{
+	console.log("Retrieving next month");
+}
+
